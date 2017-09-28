549fce01a9fea228c7dca2faff59892eda2097a2
tewan2657 <tewan2657@HRH-IC0027326.wrdsb.ca>
2017-09-28 10:05:55 -0400

    Finished new lesson on SortSearching

--- src/javareview/SortingSearching.java
+++ src/javareview/SortingSearching.java
@@ -4,6 +4,8 @@
  */
 package javareview;
 
+import java.util.Arrays;
+
 /**
  *
  * @author tewan2657
@@ -15,6 +17,7 @@ public class SortingSearching {
         array[p1] = array[p2];
         array[p2] = temp;
     }
+//selectionSort
 
     public void selectionSort(int[] array) {
         //Keep track  of which position we are sorting
@@ -31,27 +34,131 @@ public class SortingSearching {
         }
 
     }
+//Bubble sort
 
-    public void bubbleSort(int[] array){
-     //Keep track of end   
-     int last = array.length;  
-     //boolean to flag a swap
-     boolean swap = true;
-     //continue if a swap has been made
-     while(swap){
-        //assume no swaps will be done 
-         swap = false;
-         //look for swaps
-         for (int i = 0; i < last; i++) {
-             
-         }
-     }
+    public void bubbleSort(int[] array) {
+        //Keep track of end   
+        int last = array.length;
+        //boolean to flag a swap
+        boolean swap = true;
+        //continue if a swap has been made
+        while (swap) {
+            //assume no swaps will be done 
+            swap = false;
+            //look for swaps
+            for (int i = 0; i < last - 1; i++) {
+                //find a bigger value?
+                if (array[i] > array[i + 1]) {
+                    //swap
+                    swap(array, i, i + 1);
+                    //set flag to true
+                    swap = true;
+                }
+
+            }
+        }
+        //move the last position tracker
+        last--;
     }
+    //insertionSort
+
+    public void insertionSort(int[] array) {
+        //start going through the array 
+        for (int i = 0; i < array.length - 1; i++) {
+            //store position    
+            int position = i;
+            // check values beside each other
+            while (position >= 0 && array[position] > array[position + 1]) {
+                // if out of place, swap it downwards
+                // until the correct position is reached  
+                swap(array, position, position + 1);
+                position--;
+            }
+        }
+
+    }
+
+    public void mergeSort(int[] array) {
+        //if we are 1 item. done sorting
+        if (array.length <= 1) {
+            return;
+        }
+        //divide into 2 arrays
+        int split = array.length / 2;
+        //create 2 arrays
+        int[] front = Arrays.copyOfRange(array, 0, split);
+        int[] back = Arrays.copyOfRange(array, split, array.length);
+
+        // recursivley sort
+        mergeSort(front);
+        mergeSort(back);
+
+        // put numbers in correct spot
+        int fSpot = 0;
+        int bSpot = 0;
+        for (int i = 0; i < array.length; i++) {
+            // no more items in front array 
+            if (fSpot == front.length) {
+                //put item from back array in    
+                array[i] = back[bSpot];
+                bSpot++;
+
+            } else if (bSpot == back.length) {
+                //put item from back array in
+                array[i] = front[fSpot];
+                fSpot++;
+            } else if (front[fSpot] < back[bSpot]) {
+                //put from item in 
+                array[i] = front[fSpot];
+                fSpot++;
+            } else {
+                //put back item in 
+                array[i] = back[bSpot];
+                bSpot++;
+            }
+
+        }
+
+    }
+
+    public int sequentialSearch(int[] array, int number) {
+        //start going through array
+        for (int i = 0; i < array.length; i++) {
+            // did i find it?
+            if (array[i] == number) {
+                //tell them where
+                return i;
+            }
+        }
+        //didn't find it 
+        return -1;
+
+    }
+    //binary seach
+    public int binarySearch(int[] array, int target) {
+        int min = 0;
+        int max = array.length - 1;
+        while (max > min) {
+            int guess = (max + min) / 2;
+            //find it?
+            if(array[guess]==target){
+                return guess;
+            }else if(target > array[guess]){
+                min = guess + 1; 
+            }else{
+                max = guess -1;
+            }  
+        }
+        //didn't find it;
+        return -1;
+        }
+
+
+        /**
+         * @param args the command line arguments
+         */
     
-    
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
         SortingSearching test = new SortingSearching();
 
@@ -68,11 +175,12 @@ public class SortingSearching {
             System.out.println(num[i]);
         }
         //SORT here
-        test.selectionSort(num);
+        test.mergeSort(num);
         //AFTER
         System.out.println("AFTER:");
         for (int i = 0; i < num.length; i++) {
             System.out.println(num[i]);
         }
+        System.out.println("The number is in the " + test.binarySearch(num, 100)+ " spot");
     }
 }
